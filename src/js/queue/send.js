d1eca0c38f6f57f246674218047e8c273fcc08d4
kadoyau <el.psi.congroo@gmail.com>
2018-07-10 02:05:14 +0900

    RabbitMQチュートリアル：publisherの追加

---
+++ src/js/queue/send.js
@@ -0,0 +1,28 @@
+// @flow
+
+const amqp = require('amqplib');
+
+class Publisher {
+  async publish() {
+    try {
+      const conn = await amqp.connect('amqp://localhost');
+      const ch = await conn.createChannel();
+      const q = 'hello';
+      await ch.assertQueue(q, {durable: false});
+      await ch.sendToQueue(q, Buffer.from('Hello World'));
+      console.log('send!');
+
+      await ch.close();
+      await conn.close();
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
+
+
+(async () => {
+  const publisher = new Publisher();
+  return publisher.publish();
+})();
+
