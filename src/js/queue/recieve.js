5b715502d8523887c1f9f4f54356e060e3a65ad2
kadoyau <el.psi.congroo@gmail.com>
2018-07-10 02:21:42 +0900

    Consumerの追加
    https://gyazo.com/a290a26a1a9e94f9af32d9be4e1386c4

---
+++ src/js/queue/recieve.js
@@ -0,0 +1,28 @@
+// @flow
+const amqp = require('amqplib');
+
+class Consumer {
+  async consume() {
+    try {
+      const conn = await amqp.connect('amqp://localhost');
+      const ch = await conn.createChannel();
+      // publisherより先に開始するのでqueueがあるか確認する
+      const q = 'hello';
+      await ch.assertQueue(q, { durable: false });
+      await ch.consume(q, (msg) => {
+        console.log('Recv: %s', msg.content.toString());
+      }, { noAck: true });
+
+      // await ch.close();
+      // await conn.close();
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
+
+
+(async () => {
+  const consumer = new Consumer();
+  return consumer.consume();
+})();
