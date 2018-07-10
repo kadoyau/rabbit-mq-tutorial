0dac0b2196b3d5c57d0006ffcebc375f1c94fd17
kadoyau <el.psi.congroo@gmail.com>
2018-07-11 01:21:12 +0900

    2 Worker Queues: Preparation
    task_queuseに引数を渡すとその末尾の...の秒数に応じて遅延する
    デフォルトではラウンドロビン方式でワーカーに割り当てられる
    https://gyazo.com/0744b20b6192d9e37159b1ba8488ac37

---
+++ src/js/queue/task_queue.js
@@ -0,0 +1,30 @@
+// @flow
+
+const amqp = require('amqplib');
+
+class Publisher {
+  async publish() {
+    try {
+      const msg = process.argv.slice(2).join(' ') || "Hello World!";
+
+      const conn = await amqp.connect('amqp://localhost');
+      const ch = await conn.createChannel();
+      const q = 'task_queue';
+      await ch.assertQueue(q, {durable: true});
+      await ch.sendToQueue(q, Buffer.from(msg), {persistent: true});
+      console.log('Sent %s', msg);
+
+      await ch.close();
+      await conn.close();
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
+
+
+(async () => {
+  const publisher = new Publisher();
+  return publisher.publish();
+})();
+
---
+++ src/js/queue/worker.js
@@ -0,0 +1,31 @@
+// @flow
+const amqp = require('amqplib');
+
+class Consumer {
+  async consume() {
+    try {
+      const conn = await amqp.connect('amqp://localhost');
+      const ch = await conn.createChannel();
+      // publisherより先に開始するのでqueueがあるか確認する
+      const q = 'task_queue';
+      await ch.assertQueue(q, { durable: true });
+      await ch.consume(q, (msg) => {
+        const secs = msg.content.toString().split('.').length - 1;
+        console.log('Recv: %s', msg.content.toString());
+        setTimeout(() => {
+          console.log('Done');
+        }, secs * 1000);
+      }, { noAck: true });
+      // await ch.close();
+      // await conn.close();
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
+
+
+(async () => {
+  const consumer = new Consumer();
+  return consumer.consume();
+})();
