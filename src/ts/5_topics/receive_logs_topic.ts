65aeb767256dd2981cc70480b8a75b78abc7f63a
kadoyau <el.psi.congroo@gmail.com>
2018-09-11 03:49:07 +0900

    4で利用したファイルを追加

---
+++ src/ts/5_topics/receive_logs_topic.ts
@@ -0,0 +1,44 @@
+const amqp = require('amqplib');
+
+/**
+ * pub/subのsubscriber
+ * 複数起動すると適当なキューが生成される
+ * コネクションを破棄するとキューも破棄される
+ */
+class ReceiverTopic {
+  static async receive() {
+    try {
+      const args = process.argv.slice(2);
+      if (args.length === 0) {
+        console.log('Usage: receive_logs_direct.js [info] [warning] [error]');
+        process.exit(1);
+      }
+      const conn = await amqp.connect('amqp://localhost');
+      const ch = await conn.createChannel();
+
+      const exchange = 'direct_logs';
+      // type=directなexchangeを作成する
+      ch.assertExchange(exchange, 'direct', { durable: false });
+
+      const queueName = '';
+      // キュー名が空なので、RabbitMQはランダムなキュー名を返してくれる
+      // exclusive: trueなのでコネクションが閉じられるとキューが破棄される
+      const q = await ch.assertQueue(queueName, { durable: false, exclusive: true });
+      console.log('[*] Waiting for messages in %s. To exit press CTRL+C', q.queue);
+
+      // すべてのseverityとbind
+      args.forEach((severity) => {
+        ch.bindQueue(/* queue name */ q.queue, /* exchange name */ exchange, severity);
+      });
+
+      // メッセージを受け取る
+      ch.consume(q.queue, (receivedMsg: any) => {
+        console.log(' [x] %s: "%s"', receivedMsg.fields.routingKey, receivedMsg.content.toString());
+      }, { noAck: true });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
+
+ReceiverTopic.receive();
